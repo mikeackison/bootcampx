9c3e1e31b4b5a458dfb05b1694fdf128487ef935
Mike Ackison <mike.ackison@shopify.com>
2021-02-02 02:47:48 +0000

    first commit

---
+++ 3_queries_group-by/2_assignments_per_day_is_greater_than_10.sql
@@ -0,0 +1,5 @@
+SELECT day, COUNT(assignments.day) AS total_assignments
+FROM assignments
+GROUP BY day
+HAVING count(*) >= 10
+ORDER BY day;
---
+++ 3_queries_group-by/6_low_avg_completion_time.sql
@@ -0,0 +1,11 @@
+SELECT students.name AS student, 
+  AVG(assignment_submissions.duration) AS average_assignment_duration,
+  AVG(assignments.duration) as average_estimated_duration
+
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+JOIN assignments ON assignment_id = assignment_id
+WHERE end_date IS NULL
+GROUP BY student
+HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
+ORDER BY average_assignment_duration;
