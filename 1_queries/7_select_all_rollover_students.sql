26c8da93d5f10161728b78c3619122cd1865a221
Mike Ackison <mike.ackison@shopify.com>
2021-02-02 22:03:14 +0000

    add double quotes after AS in SELECT for consistancy and formating

--- 1_queries/7_select_all_rollover_students.sql
+++ 1_queries/7_select_all_rollover_students.sql
@@ -1,4 +1,4 @@
-SELECT students.name, cohorts.name, cohorts.start_date as cohort_start_date, students.start_date as student_start_date
+SELECT students.name, cohorts.name, cohorts.start_date AS "cohort_start_date", students.start_date AS "student_start_date"
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.start_date != students.start_date
